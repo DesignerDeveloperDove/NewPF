69443e8891392dd872d170e55cb4e4e73a8bdfc5
Dove <151492415+DesignerDeveloperDove@users.noreply.github.com>
2025-04-21 12:12:35 -0400

    Update Script.js

---
+++ Script.js
@@ -0,0 +1,7 @@
+document.getElementById("Tester").addEventListener("click", Test);
+
+function Test(){
+    if (event.type ==="click" )
+    console.log("js sheet works")
+    else(console.log("Event listener didnt work"))
+};
